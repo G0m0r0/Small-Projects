01a750ba68ef2e18b0c7b30229bc58c046591567
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2020-01-11 14:15:02 +0200

    Sorting algorithm- custom library

---
+++ create custom library/CustomLibrary/CustomLibrary/Program.cs
@@ -0,0 +1,32 @@
+﻿using Sorts;
+using System;
+
+namespace CustomLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rd = new Random();
+            int[] numbers = new int[5];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rd.Next(1, 6);
+            }
+
+           // Sort.IntArrayBubbleSort(numbers);
+           // Sort.IntArrayInsertionSort(numbers);
+           // Sort.IntArraySelectionSort(numbers);
+
+            foreach (var num in numbers)
+            {
+                Console.Write(num+" ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(Sort.IntArrayMin(numbers, 0));
+        }
+    }
+}
---
+++ create custom library/CustomLibrary/Sorts/Sort.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Sorts
+{
+    public class Sort
+    {
+        public static void exchange(int[] data, int m, int n)
+        {
+            int temporary;
+
+            temporary = data[m];
+            data[m] = data[n];
+            data[n] = temporary;
+        } 
+
+        public static void IntArrayBubbleSort(int[] data)
+        {
+            int i, j;
+            int N = data.Length;
+
+            for (j = N - 1; j > 0; j--)
+            {
+                for (i = 0; i < j; i++)
+                {
+                    if (data[i] > data[i + 1])
+                        exchange(data, i, i + 1);
+                }
+            }
+        }
+        public static int IntArrayMin(int[] data, int start)
+        {
+            int minPos = start;
+            for (int pos = start + 1; pos < data.Length; pos++)
+                if (data[pos] < data[minPos])
+                    minPos = pos;
+            return minPos;
+        }
+
+        public static void IntArraySelectionSort(int[] data)
+        {
+            int i;
+            int N = data.Length;
+
+            for (i = 0; i < N - 1; i++)
+            {
+                int k = IntArrayMin(data, i);
+                if (i != k)
+                    exchange(data, i, k);
+            }
+        }
+
+        public static void IntArrayInsertionSort(int[] data)
+        {
+            int i, j;
+            int N = data.Length;
+
+            for (j = 1; j < N; j++)
+            {
+                for (i = j; i > 0 && data[i] < data[i - 1]; i--)
+                {
+                    exchange(data, i, i - 1);
+                }
+            }
+        }
+
+    }
+}
