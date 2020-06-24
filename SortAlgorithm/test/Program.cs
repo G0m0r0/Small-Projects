d7f297d84e1503feb907722606b392f5e93b5523
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2020-06-24 22:07:28 +0300

    linq
    101 linq

---
+++ SortAlgorithm/test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+
+            Console.WriteLine(string.Join(" ",arr.Where(x=>x%3==0)));
+            //test
+        }
+    }
+}
