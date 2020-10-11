cd2fc3d60e71a4eb0204dd58c483004633f230f4
Delyan Dimitrov <dimitrovdelyann@gmail.com>
2020-10-11 16:33:04 +0300

    video to binary

---
+++ video to bytes/VideoTobytes/VideoTobytes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+
+namespace VideoTobytes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Programming\Video\Softuni\C# Advance\Advanced.mp4";
+            FileStream fs = null;
+            BinaryReader br = null;
+
+            fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            br = new BinaryReader(fs);
+            while (true)
+            {
+                int chunkSize = 1024; // assumed as 1kb you can extend as per your requirement
+
+                //Here is your byte array
+                byte[] buffer = br.ReadBytes(chunkSize);//reading the bytes
+
+                if (buffer.Length == 0)
+                {
+                    break;
+                }
+                BitArray bitsBuffer = new BitArray(buffer);                
+
+                DisplayBitArray(bitsBuffer);
+            }          
+            
+        }
+
+        static void DisplayBitArray(BitArray bitArray)
+        {
+            for (int i = 0; i < bitArray.Count; i++)
+            {
+                bool bit = bitArray.Get(i);
+                Console.Write(bit ? 1 : 0);
+            }
+        }
+    }
+}
